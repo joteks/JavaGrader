8d7313cadb5b1b0acb1ad24265b5d0af95cd9ab3
Robyn Berkel <rberkel@purdue.edu>
2017-09-10 17:01:39 -0400

    random updates

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,29 +1,50 @@
-
-
-/**
- * Topic 1, Project 1 : AboutMe.java
- * @author AUDREYOTT
- * 
- */
 import java.util.Scanner;
 public class AboutMe {
+
+ 
+private String myName;
+private String mySchool;
+private int myAge;
+ 
+ AboutMe(String myName, String mySchool, int myAge){
+  this.myName = myName;
+  this.mySchool = mySchool;
+  this.myAge = myAge;
+ }
+ 
+ 
+ public static void main(String[] args) {
+   
+  Scanner input = new Scanner(System.in);
   
-  public String myName() {
-	 return "berkel";
-  }
-  public String mySchool() {
-    return "hello";
-  }
-  public int myAge() {
-    return 0;
-  }
-   public static void main (String[] args) {
-     System.out.println("Hello!");
-     
-     AboutMe t = new AboutMe();
-     
-     
-     System.out.println("My name is " + t.myName() + ", and I attend " + t.mySchool() +  ". I am " + t.myAge() + " years old." );
-     System.out.println("Thank you!");
+  System.out.println("Welcome User: Please enter the information that is asked for");
+      
+  String name = input.nextLine();
+  
+  String school = input.nextLine();
+  
+  int age = Integer.parseInt(input.nextLine());
+  
+  AboutMe about = new AboutMe(name,school,age);
+      
+  System.out.println("My name is " + about.myName() + ", and I attend " + about.mySchool() + ". I am " + about.myAge()   + " years old.");
+  
+  input.close();
+
+ }
+ 
+ String myName(){
+     return myName;
    }
+   
+   
+   String mySchool(){
+     return mySchool;
+   }
+   
+   int myAge(){
+     return myAge;
+     
+   }
+
 }
--- src/Project.java
+++ src/Project.java
@@ -17,19 +17,21 @@ public class Project {
 		//define tests
 
 		TestCase[] tests = {
-				new TestCase("Test 1", "AboutMe.java compiles", "Correctness", 1),
-				new TestCase("Test 2", "Class header written correctly", "Completion", 1),
-				new TestCase("Test 3", "Main method signature written correctly", "Completion", 1),
-				new TestCase("Test 4", "myName method signature written correctly", "Completion", 1),
-				new TestCase("Test 5", "mySchool method signature written correctly", "Completion", 1),
-				new TestCase("Test 6", "myAge method signature written correctly", "Completion", 1),
-				new TestCase("Test 7", "Main method uses myAge method call", "Completion", 1),
-				new TestCase("Test 8", "Main method uses mySchool method call", "Completion", 1),
-				new TestCase("Test 9", "Main method uses myName method call", "Completion", 1),
-				new TestCase("Test 10", "Main method greets user", "Correctness", 1),
-				new TestCase("Test 11", "myName functionality", "Correctness", 1),
-				new TestCase("Test 12", "mySchool functionality", "Correctness", 1),
-				new TestCase("Test 13", "myAge functionality", "Correctness", 1)
+				new TestCase("Test 1", "AboutMe.java exists in student directory", "Completion", 1),
+				new TestCase("Test 2", "AboutMe.java compiles", "Correctness", 1),
+				new TestCase("Test 3", "Class header written correctly", "Completion", 1),
+				new TestCase("Test 4", "Main method signature written correctly", "Completion", 1),
+				new TestCase("Test 5", "myName method signature written correctly", "Completion", 1),
+				new TestCase("Test 6", "mySchool method signature written correctly", "Completion", 1),
+				new TestCase("Test 7", "myAge method signature written correctly", "Completion", 1),
+				new TestCase("Test 8", "Main method uses myAge method call", "Completion", 1),
+				new TestCase("Test 9", "Main method uses mySchool method call", "Completion", 1),
+				new TestCase("Test 10", "Main method uses myName method call", "Completion", 1),
+				new TestCase("Test 11", "Main method greets user", "Correctness", 1),
+				new TestCase("Test 12", "AboutMe object created in main method", "Completion", 1),
+				new TestCase("Test 13", "myName functionality", "Correctness", 1),
+				new TestCase("Test 14", "mySchool functionality", "Correctness", 1),
+				new TestCase("Test 15", "myAge functionality", "Correctness", 1)
 			};
 
 		//Almost every method throws an exception, just put it all under here
@@ -37,6 +39,14 @@ public class Project {
 		try {
 			String work = "AboutMe.java";
 			String solution = "asnlib/AboutMe.java";
+
+			File studentwork = new File(work);
+			if (!studentwork.exists()) {
+				System.out.println("AboutMe.java was not found. Did you name your file correctly?");
+				return;
+			}
+
+			tests[0].setResult(true);
 
 			//Report problems
 			Parser parser = new Parser();
@@ -49,8 +59,8 @@ public class Project {
 				return;
 			}
 
-			tests[0].setResult(TestCase.compile(new File(work)));
-			tests[1].setResult(true);
+			tests[1].setResult(TestCase.compile(new File(work)));
+			tests[2].setResult(true);
 
 			//replace methods and create temporary files
 			String[] sigs = {"void main(String[])", "String myName()", "String mySchool()", "int myAge()"};
@@ -61,14 +71,17 @@ public class Project {
 				if (files[i] == null) {
 					System.out.println("Method not found: " + sigs[i]);
 				} else {
-					tests[i + 2].setResult(true);
+					tests[i + 3].setResult(true);
 				}
 			}
 
 			//Test myName, myAge, mySchool
-			tests[10].setResult(TestCase.runMain(files[1], null, "(?s).*My name: \\D+.*"));
-			tests[11].setResult(TestCase.runMain(files[2], null, "(?s).*My school: \\D+.*"));
-			tests[12].setResult(TestCase.runMain(files[3], null, "(?s).*My age: \\d+.*"));
+			tests[12].setResult(TestCase.runMain(files[1], null, "(?s).*My name: \\D+.*"));
+			tests[13].setResult(TestCase.runMain(files[2], null, "(?s).*My school: \\D+.*"));
+			tests[14].setResult(TestCase.runMain(files[3], null, "(?s).*My age: \\d+.*"));
+
+			//Test for Extra Credit
+
 
 			List<Expression> exs = parser.findExpressionsOfType("void main(String[])", MethodCallExpr.class);
 
@@ -76,11 +89,19 @@ public class Project {
 				if (ex instanceof MethodCallExpr) {
 					MethodCallExpr mx = (MethodCallExpr)ex;
 					String name = mx.getName().asString();
-					if (name.equals("myAge")) tests[6].setResult(true);
-					if (name.equals("mySchool")) tests[7].setResult(true);
-					if (name.equals("myName")) tests[8].setResult(true);
+					if (name.equals("myAge")) tests[7].setResult(true);
+					if (name.equals("mySchool")) tests[8].setResult(true);
+					if (name.equals("myName")) tests[9].setResult(true);
 					if (name.equals("println") || mx.getName().equals("printf") || mx.getName().equals("print") || mx.getName().equals("write"))
-						tests[9].setResult(true);
+						tests[10].setResult(true);
+				}
+			}
+
+			for (Expression ex : exs) {
+				if (ex instanceof ObjectCreationExpr) {
+					ObjectCreationExpr ox = (ObjectCreationExpr)ex;
+					ClassOrInterfaceType c = ox.getType();
+					if (c.getName().asString().equals("AboutMe")) tests[11].setResult(true);
 				}
 			}
 
